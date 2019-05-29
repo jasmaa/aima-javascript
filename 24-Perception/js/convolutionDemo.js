1e49a41ff7236560b52795d6b674cb614b742baf
jasmaa <jasonmaa3955@gmail.com>
2019-05-29 08:17:13 -0400

    Finish convolution demo

--- 24-Perception/js/convolutionDemo.js
+++ 24-Perception/js/convolutionDemo.js
@@ -1,4 +1,4 @@
-// UI
+// Convolution demo
 
 const e = React.createElement;
 
@@ -21,7 +21,9 @@ class Cell extends React.Component {
 
         return e('div', {
                 className:'square',
-                style: {backgroundColor: bgColor}
+                style: {
+                    'background-color': bgColor,
+                }
             },
             e('div', {style: {color: textColor}},
                 this.props.value
@@ -31,7 +33,51 @@ class Cell extends React.Component {
 }
 
 /**
- * Grid UI for convolution demo
+ * Convolution result grid
+ */
+class ConvolutionResult extends React.Component {
+
+    renderCells(){
+        let cells = []
+        for(let i=0; i < 5; i++){
+            for(let j=0; j < 5; j++){
+
+                let value = 0;
+                let isTarget = this.props.filterData.x == j && this.props.filterData.y == i;
+
+               // Perform 2D convolution
+                for(let rowOffset=-parseInt(this.props.filterData.h / 2); rowOffset <= parseInt(this.props.filterData.h / 2); rowOffset++){
+                    for(let colOffset=-parseInt(this.props.filterData.w / 2); colOffset <= parseInt(this.props.filterData.w / 2); colOffset++){
+                        if(i + rowOffset >= 0 && i + rowOffset < 5 && j + colOffset >= 0 && j + colOffset < 5){
+                            let filterRow = this.props.filterData.h - (rowOffset + parseInt(this.props.filterData.h / 2)) - 1;
+                            let filterCol = this.props.filterData.w - (colOffset + parseInt(this.props.filterData.w / 2)) - 1;
+                            value += this.props.source[i+rowOffset][j+colOffset] * this.props.filter[filterRow][filterCol];
+                        }
+                    }
+                }
+
+                cells.push(e(Cell, {
+                    value: value,
+                    isHighlighted: isTarget,
+                }, null));
+            }
+        }
+
+        return cells;
+    }
+
+    render(){
+        return e('div', {
+                className:'square-grid-5'
+            },
+            this.renderCells()
+        );
+    }
+
+}
+
+/**
+ * Convolution flter application grid
  */
 class ConvolutionGrid extends React.Component {
 
@@ -40,13 +86,17 @@ class ConvolutionGrid extends React.Component {
         for(let i=0; i < 5; i++){
             for(let j=0; j < 5; j++){
 
-                // Perform convolution if within filter bounds
-                let isWithinFilter = i >= this.props.filterData.y && i < this.props.filterData.y + this.props.filterData.h &&
-                                        j >= this.props.filterData.x && j < this.props.filterData.x + this.props.filterData.w;
+                // Highlight cells
+                let isWithinFilter =    i >= this.props.filterData.y - parseInt(this.props.filterData.h / 2) &&
+                                        i <= this.props.filterData.y + parseInt(this.props.filterData.h / 2) &&
+                                        j >= this.props.filterData.x - parseInt(this.props.filterData.w / 2)&&
+                                        j <= this.props.filterData.x + parseInt(this.props.filterData.w / 2);
 
                 let value = this.props.source[i][j];
                 if(isWithinFilter){
-                    value *= this.props.filter[3 - (i - this.props.filterData.y) - 1][3 - (j - this.props.filterData.x) - 1];
+                    let filterRow = this.props.filterData.h - (i - this.props.filterData.y + parseInt(this.props.filterData.h / 2)) - 1;
+                    let filterCol = this.props.filterData.w - (j - this.props.filterData.x + parseInt(this.props.filterData.w / 2)) - 1;
+                    value *= this.props.filter[filterRow][filterCol];
                 }
 
                 cells.push(e(Cell, {
@@ -91,7 +141,7 @@ class ConvolutionControl extends React.Component {
 }
 
 /**
- * Filter input UI
+ * Filter input grid
  */
 class ConvolutionFilterInput extends React.Component {
 
@@ -120,7 +170,7 @@ class ConvolutionFilterInput extends React.Component {
 }
 
 /**
- * Source input UI
+ * Source input grid
  */
 class ConvolutionSourceInput extends React.Component {
 
@@ -183,17 +233,13 @@ class ConvolutionDemo extends React.Component {
     reset(){
         this.filter = [
             [1, 2, 1],
-            [3, 4, 5],
-            [6, 7, 8],
+            [2, 4, 2],
+            [1, 2, 1],
         ]
         this.filterData = {x: 0, y: 0, w: 3, h: 3},
-        this.source = [
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-        ]
+
+        // Generate 5x5 array of random values
+        this.source = Array.from({length: 5}, () => Array.from({length: 5}, () => Math.floor(Math.random() * 10)));
 
         this.setState({
             filter: this.filter,
@@ -210,7 +256,21 @@ class ConvolutionDemo extends React.Component {
      * @param {integer} col 
      */
     updateData(data, value, row, col){
-        data[row][col] = +value;
+
+        // screen values
+        if(isNaN(value)){
+            return;
+        }
+
+        let clampedValue = +value;
+        if(clampedValue >= 10){
+            return;
+        }
+        else if(clampedValue < 0){
+            return;
+        }
+
+        data[row][col] = clampedValue;
 
         this.setState({
             filter: this.filter,
@@ -221,33 +281,47 @@ class ConvolutionDemo extends React.Component {
 
     render(){
         return e('div', null, [
-            e(ConvolutionControl, {
-                moveHandler: (x, y)=>this.move(x, y),
-                resetHandler: ()=>this.reset(),
-            }, null),
-            e('br', null, null),
             e('br', null, null),
             e('div', {className: 'row'}, [
-                e('div', {className: 'col-md-4'},
+                e('div', {className: 'col-md-4'}, [
+                    e('h4', {align: 'center'}, "Filter"),
                     e(ConvolutionFilterInput, {
                         filter: this.filter,
                         updateFilterHandler: (v, i, j)=>this.updateData(this.filter, v, i, j)
                     }, null)
+                ]),
+                e('div', {className: 'col-md-4'}, 
+                    e(ConvolutionControl, {
+                        moveHandler: (x, y)=>this.move(x, y),
+                        resetHandler: ()=>this.reset(),
+                    }, null)
                 ),
-                e('div', {className: 'col-md-4'},
+                e('div', {className: 'col-md-4'}, [
+                    e('h4', {align: 'center'}, "Source"),
                     e(ConvolutionSourceInput, {
                         source: this.source,
                         updateSourceHandler: (v, i, j)=>this.updateData(this.source, v, i, j)
                     }, null)
-                ),
-                e('div', {className: 'col-md-4'},
+                ]),
+            ]),
+            e('div', {className: 'row'}, [
+                e('div', {className: 'col-md-6'}, [
+                    e('h4', {align: 'center'}, "Applied"),
                     e(ConvolutionGrid, {
                         filter: this.filter,
                         filterData: this.filterData,
                         source: this.source,
                     }, null)
-                ),
-            ]),
+                ]),
+                e('div', {className: 'col-md-6'}, [
+                    e('h4', {align: 'center'}, "Result"),
+                    e(ConvolutionResult, {
+                        filter: this.filter,
+                        filterData: this.filterData,
+                        source: this.source,
+                    }, null)
+                ]),
+            ])
         ]);
     }
 }
